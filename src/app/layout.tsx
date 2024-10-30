a32a019af00bff2ea840f625636e94b088bc52ce
VishwanathIntecAi <vishwanath@intecai.in>
2024-10-30 22:41:12 +0530

    COOL

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "DIWALI WISHES",
-  description: "I HAVE DECIDED TO WISH EVERYONE ON A DIGITALIZED WAY ",
+  description: "A WEB CREATED FOR DIWALI WISHES TO PEOPLE WHO ARE FAR AWAY FROM YOU ",
 };
 
 export default function RootLayout({
